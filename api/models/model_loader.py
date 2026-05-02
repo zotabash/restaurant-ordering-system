0940ce2859d1f8cc4e162d1b6f5be6b58c2ae4b4
Raki Wane <rwane1801@outlook.com>
2026-05-02 15:37:43 -0400

    Add imports for Sandwich model in model_loader.py

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,7 +1,11 @@
 from .orders import Order
 from .order_details import OrderDetail
 from .sandwiches import Sandwich   # ← ADD THIS
-
+from .recipes import Recipe
+from .resources import Resource
+from .payments import Payment
+from .reviews import Review
+from .promo_codes import PromoCode
 from ..dependencies.database import Base, engine
 
 
