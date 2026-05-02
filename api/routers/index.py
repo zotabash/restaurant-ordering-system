bd625a5c47b598d436439be0e2d6fd436734529d
Raki Wane <rwane1801@outlook.com>
2026-05-02 16:01:40 -0400

    Add new routers for payments, reviews, and promo codes

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,6 +1,9 @@
-from . import orders, order_details
+from . import orders, order_details, payments, reviews, promo_codes
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
+    app.include_router(payments.router)
+    app.include_router(reviews.router)
+    app.include_router(promo_codes.router)
